9a9abab08762394218e55b7c87a30a972b7e4656
andai78 <tyrel92@hotmail.com>
2018-04-13 00:13:54 +0200

    display table of pokemons

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Pokemon } from './pokemon';
+import { POKEMONS } from './mock-pockemon';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
   title = 'app';
+  pokemons:Pokemon[] = [];
+
+  constructor() {
+    
+  }
+
+  ngOnInit(){
+    this.pokemons = POKEMONS;
+    console.log(this.pokemons)
+  }
 }
