4995de838865c790e456f2d95a2ca64352665086
andai78 <tyrel92@hotmail.com>
2018-04-13 21:26:25 +0200

    add directive border-card

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { BorderCardDirective } from './border-card.directive';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BorderCardDirective
   ],
   imports: [
     BrowserModule
---
+++ src/app/border-card.directive.ts
@@ -0,0 +1,38 @@
+import { Directive, ElementRef, HostListener, Input } from '@angular/core';
+
+@Directive({
+  selector: '[pknmBorderCard]'
+})
+export class BorderCardDirective {
+
+  @Input('pknmBorderCard') borderColor:string;
+  private initialColor: string = '#f5f5f5';
+	private defaultColor: string = '#009688';
+  private defaultHeight: number = 180;
+  
+  constructor(private el:ElementRef) 
+  {
+    this.setBorder(this.initialColor);
+    this.setHeight(this.defaultHeight);
+  }
+
+  @HostListener('mouseenter') onMouseEnter() {
+		this.setBorder(this.borderColor || this.defaultColor);
+	}
+
+	@HostListener('mouseleave') onMouseLeave() {
+		this.setBorder(this.initialColor);
+	}
+
+  private setBorder(color:string)
+  {
+    let border = 'solid 4px'+ color;
+    this.el.nativeElement.style.border = border;
+  }
+
+  private setHeight(height:number)
+  {
+    this.el.nativeElement.style.height = height;
+  }
+
+}
