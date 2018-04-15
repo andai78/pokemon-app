0ad1871f847448856aacf631d24c7a6878d6d308
andai78 <tyrel92@hotmail.com>
2018-04-15 14:11:23 +0200

    add custom pipe

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { BorderCardDirective } from './border-card.directive';
+import { PkmTypeColorPipe } from './pkm-type-color.pipe';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    BorderCardDirective
+    BorderCardDirective,
+    PkmTypeColorPipe
   ],
   imports: [
     BrowserModule
---
+++ src/app/pkm-type-color.pipe.spec.ts
@@ -0,0 +1,8 @@
+import { PkmTypeColorPipe } from './pkm-type-color.pipe';
+
+describe('PkmTypeColorPipe', () => {
+  it('create an instance', () => {
+    const pipe = new PkmTypeColorPipe();
+    expect(pipe).toBeTruthy();
+  });
+});
